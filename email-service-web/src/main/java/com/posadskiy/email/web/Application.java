df0a04d9aff4e556ccd303db3171af251cc8d351
Dimitri Posadskiy <dmitry.posadsky@gmail.com>
2025-07-04 17:03:51 +0200

    v0.1.1

--- email-service-web/src/main/java/com/posadskiy/email/web/Application.java
+++ email-service-web/src/main/java/com/posadskiy/email/web/Application.java
@@ -9,7 +9,7 @@ import io.swagger.v3.oas.annotations.info.License;
 @OpenAPIDefinition(
     info = @Info(
         title = "email-service",
-        version = "0.1",
+        version = "0.1.1",
         description = "Simple email delivering service for sending text or HTML based messages",
         license = @License(name = "MIT", url = "https://opensource.org/license/mit"),
         contact = @Contact(url = "https://posadskiy.com", name = "Dimitri Posadskiy", email = "support@posadskiy.com")
